0046578f73a753d98bb44fd16df11541e78993f4
Tanuj2552 <tanujtvs2552@gmail.com>
2021-06-08 23:03:08 +0530

    Updated the GUI and total running of the code

--- school_solver_detector.py
+++ school_solver_detector.py
@@ -1,21 +1,20 @@
 import time
 import turtle
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import tkinter
 from tkinter import *
 from datetime import datetime
 
-def tinkerit(link):
+def tinkerit(driver, link):
     def start_it():
         print("cool doing")
         do_it(link)
+        tr.destroy()
 
     def stop_it():
         print('bad not doing')
+        logic(driver, link)
+        tr.destroy()
 
     tr = tkinter.Tk()
     tr.title("New Question!!")
@@ -34,12 +33,8 @@ def tinkerit(link):
 def do_it(link):
     print("New Question!!")
     turtle.setpos(-300,300)
-    turtle.write('New Question', font = ('Courier', 20, 'italic'))
-    turtle.setpos(-300,0)
-    turtle.write(f'Category = {link[1]}, price = {link[0]}, topic = {link[2]}',font = ('Courier', 20, 'italic'))
-    
+    turtle.write(f'Category = {link[1]}, \n price = {link[0]},\n topic = {link[2]}',font = ('Courier', 20, 'italic'))
     return
-
 
 def get_it(driver):
     l = []
@@ -57,34 +52,45 @@ def get_it(driver):
     print('Title =  ', title)
     print('At time of: ', current_time)
     print('')
+
     return l
     #category = driver.find_element_by_xpath('//*[@id="table-3684"]/tbody/tr[1]/td[2]/div')
 
+def open_it():
+    PATH = r"D:\chromedriver_win32\chromedriver.exe"
+
+    driver = webdriver.Chrome(PATH)
+
+    url = 'https://www.schoolsolver.com/account/login/?next=/'
+
+    driver.get(url)
+
+    driver.find_element_by_id('id_username').send_keys('master_educator')
+    driver.find_element_by_id('id_password').send_keys('Adminjane@20')
+
+    driver.find_element_by_xpath('//*[@id="login-form"]/div/div[3]/div[1]').click()
+
+    driver.find_element_by_xpath('/html/body/div[1]/div[3]/nav/div[2]/ul[1]/li[4]').click()
+
+    initial_link = get_it(driver)
+    return driver, initial_link
 
 
-PATH = r"D:\chromedriver_win32\chromedriver.exe"
+def logic(driver, initial_link):
+    while(True):
+        time.sleep(10)
+        driver.refresh()
+        current_link = get_it(driver)
+        if(initial_link != current_link):
+            initial_link = current_link
+            tinkerit(driver, current_link)
+            return current_link
 
-driver = webdriver.Chrome(PATH)
+if(__name__ == "__main__"):
+    print("start")
+    driver, initial_link = open_it()
+    temp_link = logic(driver, initial_link)
 
-url = 'https://www.schoolsolver.com/account/login/?next=/'
 
-driver.get(url)
 
-driver.find_element_by_id('id_username').send_keys('master_educator')
-driver.find_element_by_id('id_password').send_keys('Adminjane@20')
 
-driver.find_element_by_xpath('//*[@id="login-form"]/div/div[3]/div[1]').click()
-
-driver.find_element_by_xpath('/html/body/div[1]/div[3]/nav/div[2]/ul[1]/li[4]').click()
-
-initial_link = get_it(driver)
-
-tinkerit(initial_link)
-
-while(True):
-    time.sleep(10)
-    driver.refresh()
-    current_link = get_it(driver)
-    if(initial_link != current_link):
-        initial_link = current_link
-        do_it(current_link)
